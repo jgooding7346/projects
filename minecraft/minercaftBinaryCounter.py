b01b2de945e8644d733f6e743882c5242dfab87c
jgooding7346 <jgooding7346@sohamcollege.org.uk>
2014-12-14 22:42:21 +0000

    Commented minercaftBinaryCounter.py

--- minecraft/minercaftBinaryCounter.py
+++ minecraft/minercaftBinaryCounter.py
@@ -3,7 +3,7 @@ mc = minecraft.Minecraft.create() #Finds the open Minecraft world
 
 mc.setBlocks(-30,-5,-30,30,30,30,0) #Removes all the blocks between -30,-5,-30 and 30,30,30
 
-for b in range(7):
+for b in range(7): #Loops 8 times with the iterator of b changing by 1 each time
     mc.setBlock(0,0,b,35,5)
     for b2 in range(7):
         mc.setBlock(1,0,b2,35,5)
@@ -16,7 +16,7 @@ for b in range(7):
                     for b6 in range(7):
                         mc.setBlock(5,0,b5,35,5)
                         for b7 in range(7):
-                            mc.setBlock(6,0,b,35,5)
+                            mc.setBlock(6,0,b7,35,5)
                             for b8 in range(7):
                                 mc.setBlock(7,0,b8,35,5)
                             
