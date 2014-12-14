9146a1f761c4bddf9a412f498e6596c78fbbf1d4
jgooding7346 <jgooding7346@sohamcollege.org.uk>
2014-12-14 22:12:24 +0000

    Commented piCamCapture.py

--- PiCam/piCamCapture.py
+++ PiCam/piCamCapture.py
@@ -1,8 +1,7 @@
-import picamera,time
+import picamera,time #Imports the modules picamera and time
 
-with picamera.Picamera() as camera:
-    camera.resolution = (1024,768)
-    camera.start_preview()
-
-    time.sleep(2)
-    camera.capture("PiCameraStuff.jpg")
+with picamera.Picamera() as camera: #Sets up the camera
+    camera.resolution = (1024,768) #Sets the camera resolution
+    camera.start_preview() #Starts the preview
+    time.sleep(2) #Sleeps for 2 seconds
+    camera.capture("PiCameraStuff.jpg") #Saves the image as PiCameraStuff.jpg
