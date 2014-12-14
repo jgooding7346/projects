7b564ee0e70b7437dd18e33383e41fec13c375a2
jgooding7346 <jgooding7346@sohamcollege.org.uk>
2014-12-14 22:22:11 +0000

    Commented piCamTimelapse.py

--- PiCam/piCamTimelapse.py
+++ PiCam/piCamTimelapse.py
@@ -1,15 +1,15 @@
-import time
-import picamera
-picFine = False
-while picFine != True:
-    with picamera.PiCamera() as camera:
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture
-        camera.start_preview
-        print('Captured %s' % filename)
-    picFineInput = input("Is this Picture ok?").lower()
-    if picFineInput == yes or picFineInput == y:
-        picFine = True
+import time, picamera #Imports the pi camera and time modules
+picFine = False #Assigns the variable picFine as False
+while picFine != True: #Loops whilst picFine is False
+    with picamera.PiCamera() as camera: #Sets up the picamera
+        camera.start_preview() #Starts the camera preview
+        time.sleep(2) #Sleeps for 2 seconds
+        filename = "cameraTest.jpeg" #Assigns the variable filename as "cameraTest.jpeg"
+        camera.capture(filename) #Saves the picture under the name assigned to filename
+        camera.stop_preview() #Ends the preview
+        print('Captured %s' % filename) #Outputs "Captured cameraTest.jpeg"
+    picFineInput = input("Is this Picture ok?").lower() #Asks the user if the picture is ok
+    if picFineInput == yes or picFineInput == y: #Checks if picFineInput is "yes" or "y"
+        picFine = True #Assigns the variable picFine as True
     else:
-        picFine = False
+        picFine = False #Assigns the variable picFine as False
