78e87b8da6ab91c927505b59b71bd459008e82f6
jgooding7346 <jgooding7346@sohamcollege.org.uk>
2014-12-14 22:01:37 +0000

    Commented loopTest.py

--- loopTest.py
+++ loopTest.py
@@ -1,10 +1,10 @@
-import time,mcpi.minecraft as minecraft
+import time,mcpi.minecraft as minecraft #Imports the modules time and mcpi.minecraft
 
-number=1
-while number < 5:
-    global number
-    number + 1
-    time.sleep(0.05)
+number=1 #Assigns the variable number as 1
+while number < 5: #Loops until number is equal to or more than 5
+    global number #makes number a global variable
+    number + 1 #Adds 1 to number
+    time.sleep(0.05) #Sleeps for 5% of a second
 
-if number >= 5:
-    print "Done"
+if number >= 5: #Checks if number is equal to or more than 5
+    print "Done" #Outputs "Done"
