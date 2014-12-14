cd0261951f1ff192a08ae3eebfb4ac4dbfc226ea
jgooding7346 <jgooding7346@sohamcollege.org.uk>
2014-12-14 21:53:02 +0000

    Commented piCamCapture.py

--- piCamCapture.py
+++ piCamCapture.py
@@ -1,9 +1,9 @@
-import picamera,time
+import picamera,time #Imports the modules picamera and time
 
-for run in range(4):
-    with picamera.PiCamera() as camera:
-        camera.resolution = (1024,768)
-        name = input("What is your name?")
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture(name + ".jpeg")
+for run in range(4): #Runs a loop a set number of times
+    with picamera.PiCamera() as camera: #Sets up the camera
+        camera.resolution = (1024,768) #Sets the camera resolution
+        name = input("What is your name?") #Asks for the user's name
+        camera.start_preview() #Starts the camera preview
+        time.sleep(2) #Sleeps for 2 seconds
+        camera.capture(name + ".jpeg") #Saves a picture as the user's name with the .jpeg suffix
