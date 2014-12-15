0a7915510dd9971066ac79f597455c48a1e7a5ef
jgooding7346 <jgooding7346@sohamcollege.org.uk>
2014-12-15 00:09:13 +0000

    Commented minecraftRoad.py

--- minecraft/minecraftRoad.py
+++ minecraft/minecraftRoad.py
@@ -1,5 +1,5 @@
-import mcpi.minecraft as minecraft
-mc = minecraft.Minecraft.create()
+import mcpi.minecraft as minecraft #Imports the module mcpi.minecraft
+mc = minecraft.Minecraft.create() #Locates an open Minecraft world
 
-black=7
-mc.setBlocks(-27,2,-8,-32,2,26,35,black)
+black=7 #Assigns the value 7 to the variable black
+mc.setBlocks(-27,2,-8,-32,2,26,35,black) #Places black wool blocks between -27, 2, -8 and -32, 2, 26
