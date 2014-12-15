d4f61b6b3938f0c29857f09d0435aa79f112b103
jgooding7346 <jgooding7346@sohamcollege.org.uk>
2014-12-15 00:27:15 +0000

    Update minecraftGravel.py

--- minecraft/minecraftGravel.py
+++ minecraft/minecraftGravel.py
@@ -1,6 +1,8 @@
+#SEE minecraftGravelV2.py FOR COMPLETE AND COMMENTED VERSION
+
 import time,mcpi.minecraft as minecraft,random
 mc = minecraft.Minecraft.create()
 while True: #Causes a loop to occur
     pos = mc.player.getPos() #Gets the player's location
-    mc.setBlock(random.randint(int(pos.x-10),int(pos.x+10)),pos.y+50,random.randint(int(pos.x-10),int(pos.x+10)),13)
+    mc.setBlock(random.randint(int(pos.x-10),int(pos.x+10)),pos.y+50,random.randint(int(pos.x-10),int(pos.x+10)),13) 
     time.sleep(0.05)
