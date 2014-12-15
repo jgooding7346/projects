b372d14bca23b65f96b2743f12ecc7d85a966cd3
jgooding7346 <jgooding7346@sohamcollege.org.uk>
2014-12-15 00:23:56 +0000

    Update minecraftHeightLimit.py

--- minecraft/minecraftHeightLimit.py
+++ minecraft/minecraftHeightLimit.py
@@ -1,6 +1,6 @@
-import time,mcpi.minecraft as minecraft
-mc = minecraft.Minecraft.create()
-while True:
-    pos = mc.player.getPos()
-    if pos.y < -10 or pos.y > 50:
-        mc.player.setPos(pos.x+1,20,pos.z+1)
+import time,mcpi.minecraft as minecraft #Imports the time and mcpi.minecraft
+mc = minecraft.Minecraft.create() #Locates an open Minecraft world
+while True: #Loops forever
+    pos = mc.player.getPos() #Assigns the player's position to the variable pos
+    if pos.y < -10 or pos.y > 50: #Checks if the player is below -10 or above 50 on the y axis
+        mc.player.setPos(pos.x+1,20,pos.z+1) #Moves the player to their previous position but diagonally along 1 block and at a y value of 20
